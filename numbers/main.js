101f2b16dce8ccf8a5a99b2d0c32998c8b0da45f
JaunMousa <juan.mousa@powercoders.org>
2024-04-25 19:52:51 +0200

    numbers training

---
+++ numbers/main.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Power Coders")
+    } else if (i % 3 === 0) {
+        console.log("Power")
+    } else if (i % 5 === 0) {
+        console.log("coders")
+    }
+}
